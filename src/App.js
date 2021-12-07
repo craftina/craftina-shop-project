d667a0138847b43aad459ab34278612b3a457f91
craftina <hristina.rum.hristova@gmail.com>
2021-12-07 20:07:25 +0200

    register component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-// import Register from "./components/Register/Register";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <Home /> */}
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,16 @@
-import './Login.css';
-const Login = () => {
+import './Register.css';
+const Register = () => {
     return (
         <div>
             <div className="screen-1">
                 <div className="logo">
                     <img src="./images/form_logo.png" />
+                </div>
+                <div className="input-box email">
+                    <label for="email">Email Address</label>
+                    <div>
+                        <input type="email" name="email" placeholder="Username@gmail.com" />
+                    </div>
                 </div>
                 <div className="input-box username">
                     <label for="username">Username</label>
@@ -18,11 +24,11 @@ const Login = () => {
                         <input className="pas" type="password" name="password" placeholder="············" />
                     </div>
                 </div>
-                <button className="login-btn">Login</button>
-                <div className="form-footer">Don't have a registration:<br /><span>Register</span></div>
+                <button className="login-btn">Register</button>
+                <div className="form-footer">You have a registration:<br /><span>Login</span></div>
             </div>
         </div>
     );
 }
 
-export default Login;
+export default Register;
