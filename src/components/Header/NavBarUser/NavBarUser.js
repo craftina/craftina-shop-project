d1b4a648805020337635fcb64ffb47ddf6fddcd8
craftina <hristina.rum.hristova@gmail.com>
2021-12-26 20:55:41 +0200

    adding link to cart

--- src/components/Header/NavBarUser/NavBarUser.js
+++ src/components/Header/NavBarUser/NavBarUser.js
@@ -35,6 +35,7 @@ const NavBarUser = () => {
         return(
         <ul className='user'>
            <li className="logout-link"><Link to="/" onClick={onLogout}>Logout</Link></li>
+           <li ><Link to="/">Cart</Link></li>
            {/* <li className="register-link"><Link to="/register">Register</Link></li> */}
         </ul>
         );
